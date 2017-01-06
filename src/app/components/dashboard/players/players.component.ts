2a24716fe90b87f28b515a060ee63669a28b29f9
Verasztó László <Veraszt Lszl>
2017-01-07 00:44:53 +0100

    design dashboard

--- src/app/components/dashboard/players/players.component.ts
+++ src/app/components/dashboard/players/players.component.ts
@@ -10,6 +10,7 @@ export class PlayersComponent implements OnInit {
 
   private socket = null;
   private notPlayingUsers = [];
+  text = "There is no available player!";
 
   constructor(
     private socketService: SocketService) {
@@ -21,6 +22,20 @@ export class PlayersComponent implements OnInit {
     me.socket.on('user.listed', function(data) {
       console.log("listed", data);
       me.notPlayingUsers = data;
+      if(data && data.length) {
+        me.text = "List of available player(s). Choose your opponent!";
+      }
+      else {
+        me.text = "There is no available player!";
+      }
+    });
+
+    me.socket.on("global.user.added", function(data) {
+      console.log("global.user.added", data);
+      if(data && data.userName) {
+        me.notPlayingUsers.push(data.userName);
+        me.text = "List of available player(s). Choose your opponent!";
+      }
     });
 
     me.refresh();
